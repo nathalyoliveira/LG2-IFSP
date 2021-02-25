189de41fc5ce9ec4e27493624b969b2eecd3ea06
Nathaly Oliveira <73591609+nathalyoliveira@users.noreply.github.com>
2021-02-25 11:12:37 -0300

    Commit Exceptions

---
+++ ExcecoesControleErros/src/Exceptions_stacktrace_throws.java
@@ -0,0 +1,22 @@
+
+public class Exceptions_stacktrace_throws {
+
+	public static void main(String[] args) {
+		
+		int[] numeros = {4, 8, 16, 32, 64, 128};
+		int[] denom = {2, 0, 4, 8, 0};
+		
+		for(int i = 0; i < numeros.length; i++) {
+			try {
+				System.out.println(numeros[i] + "/" + denom[i] + " = " + (numeros[i]/denom[i]));
+			}
+			catch(Exception e) {
+				System.out.println(e.getLocalizedMessage());
+				e.printStackTrace();
+			}
+			
+		}
+
+	}
+
+}
